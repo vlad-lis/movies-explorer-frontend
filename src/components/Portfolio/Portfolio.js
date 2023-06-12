b5e8e2e29dcaaa98db5df3ca93755e64b60f5b69
vladislav lis <lis.vladislav@gmail.com>
2023-06-12 05:48:31 +0300

    Changed projects links

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,11 +5,11 @@ function Portfolio() {
     <section className='portfolio'>
       <h3 className='portfolio__title'>{portfolio.name}</h3>
       <ul className='portfolio__list'>
-        <li className='portfolio__project'>
-          <a className='portfolio__project-link' href='https://github.com/vlad-lis/how-to-learn' target='_blank' rel='noreferrer'>
-            <p className='portfolio__project-name'>{portfolio.project1}</p>
+      <li className='portfolio__project'>
+          <a className='portfolio__project-link' href='https://github.com/vlad-lis/react-mesto-api-full-gha' target='_blank' rel='noreferrer'>
+            <p className='portfolio__project-name'>{portfolio.project3}</p>
           </a>
-          <a className='portfolio__btn' href='https://github.com/vlad-lis/how-to-learn' target='_blank' rel='noreferrer' />
+          <a className='portfolio__btn' href='https://github.com/vlad-lis/react-mesto-api-full-gha' target='_blank' rel='noreferrer' />
         </li>
         <li className='portfolio__project'>
           <a className='portfolio__project-link' href='https://github.com/vlad-lis/russian-travel' target='_blank' rel='noreferrer'>
@@ -18,10 +18,10 @@ function Portfolio() {
           <a className='portfolio__btn' href='https://github.com/vlad-lis/russian-travel' target='_blank' rel='noreferrer' />
         </li>
         <li className='portfolio__project'>
-          <a className='portfolio__project-link' href='https://github.com/vlad-lis/react-mesto-api-full-gha' target='_blank' rel='noreferrer'>
-            <p className='portfolio__project-name'>{portfolio.project3}</p>
+          <a className='portfolio__project-link' href='https://github.com/vlad-lis/how-to-learn' target='_blank' rel='noreferrer'>
+            <p className='portfolio__project-name'>{portfolio.project1}</p>
           </a>
-          <a className='portfolio__btn' href='https://github.com/vlad-lis/react-mesto-api-full-gha' target='_blank' rel='noreferrer' />
+          <a className='portfolio__btn' href='https://github.com/vlad-lis/how-to-learn' target='_blank' rel='noreferrer' />
         </li>
       </ul>
     </section>
